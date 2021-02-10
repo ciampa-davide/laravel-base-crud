7d109d87cffa7c8570dd3b5a9545004ede0a630a
Davide <ciampad06@gmail.com>
2021-02-10 17:35:51 +0100

    Creazione lato frontend laravel

---
+++ database/migrations/2021_02_10_153038_create_beers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBeersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('beers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 50);
+            $table->string('country', 50);
+            $table->string('manufacturer', 50);
+            $table->float('price', 4, 2);
+            $table->string('isbn', 13);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('beers');
+    }
+}
